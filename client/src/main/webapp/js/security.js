cff7dbc85ba76e9700c6c5d78bfe418b9e79276e
pedroigor <pigor.craveiro@gmail.com>
2012-09-20 17:20:52 -0300

    Fix for the JS redirect problem.

--- client/src/main/webapp/js/security.js
+++ client/src/main/webapp/js/security.js
@@ -62,41 +62,6 @@ $(document).ready(function() {
 	
 });
 
-
-var popup = null;
-
-$(document).ready(function() {
-
-	//Facebook signin
-	$('#facebook-sign').click(function(e) {
-        e.preventDefault();
-        popup = window.open("/todo-server/facebook", "name", "height=512, width=512");
-        popup.focus();
-        
-        popup.onload = function(){
-            if(popup.document.body.innerHTML.indexOf("true") > -1){
-            	popup.close();
-            	var jqxhr = $.ajax('/todo-server/facebook', {
-        			contentType: "application/json",
-                    dataType:'json',
-                    type:'POST', 
-                    success:function (data) {
-                        if (data.loggedIn) {
-                        	storeToken(data.token);
-        					window.location = getHost() + "/index.html";
-                        } else {
-                        	$('#login-msg').text("Authentication failed. Try again ...");
-                        }
-                    }
-                });
-    			//window.location = getHost() + "/index.html";
-            }
-        };
-        
-		return false; // prevents submit of the form
-	});
-});
-
 $(document).ready(function() {
 	if (!$('#logout-btn')) {
 		return;
@@ -126,4 +91,42 @@ $(document).ready(function() {
         	$('#userinfo-msg').text("Welcome " + data.fullName + ". Your roles are: " + data.roles);
         }
     });
+});
+
+var popup = null;
+
+function sendMainPage() {
+	popup.close();
+	window.location = getHost() + "/index.html";
+}
+
+$(document).ready(function() {
+
+	//Facebook signin
+	$('#facebook-sign').click(function(e) {
+        e.preventDefault();
+        popup = window.open("/todo-server/facebook", "name", "height=512, width=512");
+        popup.focus();
+        popup.window.reload = function(){
+        	if(popup.document.body.innerHTML.indexOf("true") > -1){
+            	popup.close();
+            	var jqxhr = $.ajax('/todo-server/facebook', {
+        			contentType: "application/json",
+                    dataType:'json',
+                    type:'POST', 
+                    success:function (data) {
+                        if (data.loggedIn) {
+                        	storeToken(data.token);
+        					window.location = getHost() + "/index.html";
+                        } else {
+                        	$('#login-msg').text("Authentication failed. Try again ...");
+                        }
+                    }
+                });
+    			//window.location = getHost() + "/index.html";
+        	}
+        };
+        
+		return false; // prevents submit of the form
+	});
 });
