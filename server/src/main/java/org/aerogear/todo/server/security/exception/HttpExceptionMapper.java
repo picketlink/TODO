73d8bd9ac58540e8f3b7bff49dc49b3900c3056b
Bruno Oliveira <bruno@abstractj.org>
2012-09-20 11:51:38 -0300

    Mapping exception responses

---
+++ server/src/main/java/org/aerogear/todo/server/security/exception/HttpExceptionMapper.java
@@ -0,0 +1,50 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.aerogear.todo.server.security.exception;
+
+import org.apache.deltaspike.core.api.exception.control.event.ExceptionToCatchEvent;
+import org.apache.deltaspike.security.api.authorization.AccessDeniedException;
+
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class HttpExceptionMapper implements ExceptionMapper<Throwable> {
+
+    @Inject
+    private Event<ExceptionToCatchEvent> event;
+
+    @Override
+    public Response toResponse(Throwable exception) {
+
+        Throwable exceptionCause = exception.getCause();
+
+        if (exceptionCause instanceof AccessDeniedException) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity(createMessage("User authentication failed")).build();
+        } else {
+            return Response.ok().build();
+        }
+    }
+
+    private String createMessage(String message){
+        return String.format("{message : %s }", message);
+    }
+}
