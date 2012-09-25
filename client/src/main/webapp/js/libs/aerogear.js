f7828b4d6d027d90bc4d889ec90036f3416f30e7
Kris Borchers <kris.borchers@gmail.com>
2012-09-25 16:05:57 -0500

    Update to latest aerogear.js

--- client/src/main/webapp/js/libs/aerogear.js
+++ client/src/main/webapp/js/libs/aerogear.js
@@ -1,4 +1,4 @@
-/*! AeroGear JavaScript Library - v1.0.0.Alpha - 2012-09-23
+/*! AeroGear JavaScript Library - v1.0.0.Alpha - 2012-09-25
 * https://github.com/aerogear/aerogear-js
 * JBoss, Home of Professional Open Source
 * Copyright 2012, Red Hat, Inc., and individual contributors
@@ -386,6 +386,7 @@
                     type: "GET",
                     success: success,
                     error: error,
+                    statusCode: options.statusCode,
                     complete: options.complete
                 };
 
@@ -486,6 +487,7 @@
                     url: url,
                     success: success,
                     error: error,
+                    statusCode: options.statusCode,
                     complete: options.complete
                 };
 
@@ -554,6 +556,9 @@
                     if ( toRemove.error && !options.error ) {
                         options.error = toRemove.error;
                     }
+                    if ( toRemove.statusCode && !options.statusCode ) {
+                        options.statusCode = toRemove.statusCode;
+                    }
                 }
 
                 delPath = delId ? "/" + delId : "";
@@ -605,6 +610,7 @@
                     url: url,
                     success: success,
                     error: error,
+                    statusCode: options.statusCode,
                     complete: options.complete
                 };
 
@@ -962,7 +968,8 @@
             type: "rest",
             name: moduleName,
             agAuth: !!settings.agAuth,
-            baseURL: settings.baseURL || null,
+            baseURL: settings.baseURL,
+            tokenName: settings.tokenName || "token",
             /**
              * aerogear.auth.adapters.rest#register( data[, options] ) -> Object
              * - data (Object): User profile to register
@@ -974,21 +981,20 @@
 
                 var that = this,
                     success = function( data, textStatus, jqXHR ) {
-                        sessionStorage.setItem( "ag-auth-" + that.name, that.agAuth ? data[ "Auth-Token" ] : "true" );
+                        sessionStorage.setItem( "ag-auth-" + that.name, that.agAuth ? data[ that.tokenName ] : "true" );
 
                         if ( options.success ) {
                             options.success.apply( this, arguments );
                         }
                     },
                     extraOptions = {
+                        contentType: options.contentType,
+                        dataType: options.dataType,
                         success: success,
+                        error: options.error,
                         data: data
                     },
                     url = "";
-
-                if ( options.error ) {
-                    extraOptions.error = options.error;
-                }
 
                 if ( options.baseURL ) {
                     url = options.baseURL;
@@ -1018,14 +1024,17 @@
 
                 var that = this,
                     success = function( data, textStatus, jqXHR ) {
-                        sessionStorage.setItem( "ag-auth-" + that.name, that.agAuth ? data[ "token" ] : "true" );
+                        sessionStorage.setItem( "ag-auth-" + that.name, that.agAuth ? data[ that.tokenName ] : "true" );
 
                         if ( options.success ) {
                             options.success.apply( this, arguments );
                         }
                     },
                     extraOptions = {
-                        success: success
+                        contentType: options.contentType,
+                        dataType: options.dataType,
+                        success: success,
+                        data: data
                     },
                     url = "";
 
@@ -1045,15 +1054,6 @@
                 }
                 if ( url.length ) {
                     extraOptions.url = url;
-                }
-
-                if ( this.agAuth ) {
-                    extraOptions.headers = {
-                        "Auth-Credential": data.credential,
-                        "Auth-Password": data.password
-                    };
-                } else {
-                    extraOptions.data = data;
                 }
 
                 return $.ajax( $.extend( {}, settings, { type: "POST" }, extraOptions ) );
@@ -1094,9 +1094,8 @@
                 }
 
                 if ( this.agAuth ) {
-                    extraOptions.headers = {
-                        "Auth-Token": sessionStorage.getItem( "ag-auth-" + this.name )
-                    };
+                    extraOptions.headers = {};
+                    extraOptions.headers[ this.tokenName ] = sessionStorage.getItem( "ag-auth-" + this.name );
                 }
 
                 return $.ajax( $.extend( {}, settings, { type: "POST" }, extraOptions ) );
@@ -1108,7 +1107,9 @@
             },
 
             addAuth: function( settings ) {
-                return $.extend( {}, settings, { headers: { "Auth-Token": sessionStorage.getItem( "ag-auth-" + this.name ) } } );
+                settings.headers = {};
+                settings.headers[ this.tokenName ] = sessionStorage.getItem( "ag-auth-" + this.name );
+                return $.extend( {}, settings );
             },
 
             deauthorize: function() {
