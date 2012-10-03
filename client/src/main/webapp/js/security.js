4fe4a9dda2678f81084548a8f63183ef79bce567
Anil Saldhana <Anil.Saldhana@jboss.com>
2012-10-03 15:47:32 -0500

    minor changes and start of twitter login

--- client/src/main/webapp/js/security.js
+++ client/src/main/webapp/js/security.js
@@ -169,7 +169,36 @@ $(document).ready(function() {
                         }
                     }
                 });
-    			//window.location = getHost() + "/index.html";
+        	}
+        };
+        
+		return false; // prevents submit of the form
+	});
+});
+
+$(document).ready(function() {
+
+	//Twitter signin
+	$('#twitter-sign').click(function(e) {
+        e.preventDefault();
+        popup = window.open("/todo-server/twitter", "name", "height=512, width=512");
+        popup.focus();
+        popup.window.reload = function(){
+        	if(popup.document.body.innerHTML.indexOf("true") > -1){
+            	popup.close();
+            	var jqxhr = $.ajax('/todo-server/twitter', {
+        			contentType: "application/json",
+                    dataType:'json',
+                    type:'POST', 
+                    success:function (data) {
+                        if (data.loggedIn) {
+                        	storeToken(data.token);
+        					window.location = getHost() + "/index.html";
+                        } else {
+                        	$('#login-msg').text("Authentication failed. Try again ...");
+                        }
+                    }
+                });
         	}
         };
         
